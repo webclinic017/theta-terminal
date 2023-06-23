2c2729952f89b279df5669f35c48c092080fa22f
mvnchi <120330513+mvnchi@users.noreply.github.com>
2023-06-23 16:14:28 +0200

    Update opscreen.py

--- opscreen.py
+++ opscreen.py
@@ -604,7 +604,7 @@ nyse = mcal.get_calendar('NYSE')
 nyse_schedule = nyse.schedule(start_date=start_date, end_date=end_date)
 # display(nyse_schedule)
 if today_str in nyse_schedule.index:
-    x = x.loc[(x['Last Trade Date'] == pd.Timestamp(date.today()))]
+    x = x.loc[(x['Last Trade Date'] >= pd.Timestamp(date.today()))]
 else:
     x = x.loc[(x['Last Trade Date'] >= pd.Timestamp(date.today() - timedelta(days=3)))]
     # x = x[x['Last Trade Date']] >= pd.to_datetime(today_str) - timedelta(days=3)
