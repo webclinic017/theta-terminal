e736dbc710f2247aba3626d055a870151e806d0d
mvnchi <120330513+mvnchi@users.noreply.github.com>
2023-07-01 20:38:49 +0200

    Update opscreen.py

--- opscreen.py
+++ opscreen.py
@@ -214,7 +214,7 @@ with col1:
     st.write("### News")
     fnews = News()
     all_news = fnews.get_news()
-    st.markdown(df.to_markdown(all_news['news'].head(10).set_index('Date'), use_container_width=True, height=200))
+    st.markdown(all_news['news'].head(10).set_index('Date').to_markdown(use_container_width=True, height=200))
     #st.dataframe(all_news['news'].head(10).set_index('Date'), use_container_width=True, height=200)
 
 
