b8124faf1791e0383f56975eb004e43705e66f2e
mvnchi <120330513+mvnchi@users.noreply.github.com>
2023-07-01 20:01:23 +0200

    Update opscreen.py

--- opscreen.py
+++ opscreen.py
@@ -217,7 +217,8 @@ with col1:
     st.dataframe(all_news['news'].head(10).set_index('Date'), use_container_width=True, height=200)
 
 with col2:
-  st.write("##")
+  st.write("#")
+  st.write("#")
   with st.expander("Index Description"):
     st.markdown(
       """
