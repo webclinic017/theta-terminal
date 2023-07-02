19f3c5be8091d8f7287266ddddacc287f07aa390
mvnchi <120330513+mvnchi@users.noreply.github.com>
2023-07-03 00:32:30 +0200

    Update opscreen.py

--- opscreen.py
+++ opscreen.py
@@ -441,10 +441,9 @@ def color_negative_red(value):
             color = "red"
             return 'color: %s' % color
 x['ROC'] = x['ROC'].astype(str) + '%'
-x['% Day Change'] = x['% Day Change'].astype(str) + '%'
 x['Annual Yield'] = x['Annual Yield'].astype(str) + '%'
 x['IV'] = x['IV'].astype(str) + '%'
-x['Moneyness'] = x['Moneyness'].astype(str) + '%'
+#x['Moneyness'] = x['Moneyness'].astype(str) + '%'
 
 x = x.sort_values('ROC', ascending=False)
 x = x.reset_index()
