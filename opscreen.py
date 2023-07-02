281d55b87df0a3921419da844a39d62c3003db53
mvnchi <120330513+mvnchi@users.noreply.github.com>
2023-07-02 22:47:56 +0200

    Update opscreen.py

--- opscreen.py
+++ opscreen.py
@@ -380,13 +380,13 @@ x['Change'] = round(percentage_change(bars_df['open'], bars_df['close']))
 
 if type == 'puts':
     x['% OTM'] = round(percentage_change(x['strike'], x['Last Price']))
-    x['Delta'] = [mb.BS([i,j,1,k], volatility=l).putDelta for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
-    x['Theta'] = [mb.BS([i,j,1,k], volatility=l).putTheta for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
+    x['Delta'] = [mb.BS([i,j,1,k], volatility=l).putDelta.round(5) for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
+    x['Theta'] = [mb.BS([i,j,1,k], volatility=l).putTheta.round(5) for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
   
 if type == 'calls':
     x['% OTM'] = round((x['strike'] * 100 / x['Last Price'])) -100
-    x['Delta'] = [mb.BS([i,j,1,k], volatility=l).callDelta for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
-    x['Theta'] = [mb.BS([i,j,1,k], volatility=l).callTheta for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
+    x['Delta'] = [mb.BS([i,j,1,k], volatility=l).callDelta.round(5) for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
+    x['Theta'] = [mb.BS([i,j,1,k], volatility=l).callTheta.round(5) for i,j,k,l in zip(x["Last Price"], x["strike"], x["DTE"], x["impliedVolatility"])]
 
 x['impliedVolatility'] = round(x['impliedVolatility'] * 100, 2)
 x['Annual Yield'] = round((x['lastPrice'] / x['strike']) * (365 / x['DTE']) * 100, 2)
